63a9e6e6fb293e8e36ba7e6b340464a4108dffcf
Nicholas Barkote <barkotenicholas@gmail.com>
2022-09-01 21:33:42 +0300

    Added new mysql credentails

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    HOST: "ec2-3-225-213-67.compute-1.amazonaws.com",
-    USER: "bfjtshpqxyacty",
-    PASSWORD: "f2a55ecddf9531cca5911c1a527acfe239babc65a8923968e82dc087619b46ee",
+    HOST: "localhosy",
+    USER: "root",
+    PASSWORD: "root",
     DB: "authen",
-    dialect: "postgres",
+    dialect: "mysql",
     pool: {
       max: 5,
       min: 0,
